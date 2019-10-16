b4c12f000a5e5ec7faf0a8d8935d59fb4e441e21
Simranbal04 <simranbal01@gmail.com>
2019-10-16 11:24:43 -0400

    PHONE: Added template code for connecting to an API

--- Sunrise/ViewController.swift
+++ Sunrise/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func clickButtonPresses(_ sender: Any) {
+        
+        print("Button Pressed")
+        
+        //1. go to sunrise api
+        //2. wait for the website to respond
+        let websiteURL = "https://sunrise-sunset.org/"
+        
+        AF.request(websiteURL).responseJSON {
+                   (response) in
+    }
+    
 
+}
 }
 
